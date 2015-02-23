43a549b856a7d351b58698bb2349812788abe32b
Akiomi Kamakura <akiomik@gmail.com>
2015-02-23 15:43:07 +0900

    changed language to scala

--- src/AndroidScalaFileTemplateProvider.scala
+++ src/AndroidScalaFileTemplateProvider.scala
@@ -1,15 +1,20 @@
-import com.intellij.ide.fileTemplates.*;
-import icons.AndroidIcons;
-import org.jetbrains.plugins.scala.ScalaFileType;
-import javax.swing.*;
+import com.intellij.ide.fileTemplates._
+import icons.AndroidIcons
+import org.jetbrains.plugins.scala.ScalaFileType
 
-public class AndroidScalaFileTemplateProvider implements FileTemplateGroupDescriptorFactory {
-  @Override
-  public FileTemplateGroupDescriptor getFileTemplatesDescriptor() {
-    String groupName = "Android Scala";
-    Icon icon = AndroidIcons.Android;
-    FileTemplateGroupDescriptor group = new FileTemplateGroupDescriptor(groupName, icon);
-    group.addTemplate(new FileTemplateDescriptor("Activity", ScalaFileType.SCALA_FILE_TYPE.getIcon()));
-    return group;
+class AndroidScalaFileTemplateProvider extends FileTemplateGroupDescriptorFactory {
+  val templates = Seq("Activity.scala", "Fragment.scala")
+
+  override def getFileTemplatesDescriptor: FileTemplateGroupDescriptor = {
+    val groupName = "Android Scala"
+    val icon = AndroidIcons.Android
+    val group = new FileTemplateGroupDescriptor(groupName, icon)
+
+    for (template <- templates) {
+      val descriptor = new FileTemplateDescriptor(template, ScalaFileType.SCALA_FILE_TYPE.getIcon)
+      group.addTemplate(descriptor)
+    }
+
+    group
   }
 }
